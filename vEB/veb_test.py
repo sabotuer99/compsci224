ebe3b04ecc15901aa0bd1cd3d033aa049dee9231
Troy Whorten <sabotuer99@gmail.com>
2018-10-02 05:33:58 +0000

    initial commit

---
+++ vEB/veb_test.py
@@ -0,0 +1,10 @@
+import unittest
+from veb import Veb
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_ctor(self):
+        a = Veb(8)
+        
+if __name__ == '__main__':
+    unittest.main()
