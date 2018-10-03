e510d5dcbc50b4c7ff1903e185132477e8a60fb7
Troy Whorten <sabotuer99@gmail.com>
2018-10-03 06:58:24 +0000

    made some progress on veb, still failing recursive pred and succ

--- vEB/veb.py
+++ vEB/veb.py
@@ -1,21 +1,20 @@
-import math
+from math import log
+from math import ceil
 
 class Veb:
     def __init__(self, U):
-        self.U = 1 << (self.universe_size(U) - 1)
+        self.U = (1 << self.universe_width(U)) - 1
         print(self.U)
         self.max_x = None
         self.min_x = None
-        raise AssertionError()
-        if(self.U > 1):
-            m2 = 1 << int(self.universe_size(U)/ 2)
+        m2 = self.halve_universe(self.U)
+        if(m2 > 1):
             self.summary = Veb(m2)
             self.clusters = {}
-            
     
     def insert(self, x):
-        if x < 0 or x > self.U:
-            raise AssertionError("Out of bounds: " + str(x))
+        if (x < 0) or (x > self.U):
+            raise AssertionError("{} is not between 0 and {}".format(x, self.U))
         if self.min_x == None:
             self.min_x = x
             self.max_x = x
@@ -28,11 +27,9 @@ class Veb:
             self.max_x = to_insert
         c, i = self.split_bits(to_insert)
         if c not in self.clusters:
-            self.clusters[c] = Veb(self.universe_size(self.U)/2)
+            self.clusters[c] = Veb(self.halve_universe(self.U))
             self.summary.insert(c)
         self.clusters[c].insert(i)
-        
-        
         
     def pred(self, x):
         if (self.min_x == None) or x <= self.min_x:
@@ -40,12 +37,21 @@ class Veb:
         if x > self.max_x:
             return self.max_x
         c, i = self.split_bits(x)
+        hi = x - i
+        half_width = int(self.universe_width(self.U) / 2)
+        print("c: " + str(c))
+        print("i: " + str(i))
+        print("hi: " + str(hi))
+        print(self.summary.clusters)
+        print("min: " + str(self.min_x))
+        print("max: " + str(self.max_x))
+        print(c in self.clusters)
+        print(self.clusters[c].min_x)
         if c in self.clusters and self.clusters[c].min_x < x:
-            return c + self.clusters[c].pred(i)
+            return hi + self.clusters[c].pred(i)
         else:
             cid = self.summary.pred(c)
-            return c + self.clusters[cid].max_x
-
+            return hi + self.clusters[cid].max_x
             
     def succ(self, x):
         if (self.min_x == None) or x >= self.max_x:
@@ -53,21 +59,37 @@ class Veb:
         if x < self.min_x:
             return self.min_x
         c, i = self.split_bits(x)
+        hi = x - i
+        half_width = int(self.universe_width(self.U) / 2)
+        print("c: " + str(c))
+        print("i: " + str(i))
+        print("hi: " + str(hi))
+        print(self.summary.clusters)
+        print("min: " + str(self.min_x))
+        print("max: " + str(self.max_x))
+        print(c in self.clusters)
         if c in self.clusters and self.clusters[c].max_x > x:
-            return c + self.clusters[c].succ(i)
+            return hi + self.clusters[c].succ(i)
         else:
             cid = self.summary.succ(c)
-            return c + self.clusters[cid].min_x
+            return hi + self.clusters[cid].min_x
             
-    def universe_size(self, x):
-        msb = int(math.log(x, 2))
-        return 1 << math.ceil(math.log(msb, 2))
-        
+    def universe_width(self, x):
+        width = ceil(log(x, 2))
+        next_pow_2_width = ceil(log(width, 2))
+        return 1 << next_pow_2_width
     
     def split_bits(self, x):
-        m2 = int(self.universe_size(self.U) / 2)
-        mask_right =  (1 << m2) - 1
-        mask_left = mask_right << m2
-        c = x & mask_left
+        half_width = int(self.universe_width(self.U) / 2)
+        mask_right =  (1 << half_width) - 1
+        mask_left = mask_right << half_width
+        
+        print(mask_left)
+        print(mask_right)
+        
+        c = (x & mask_left) >> half_width
         i = x & mask_right
         return c, i
+        
+    def halve_universe(self, U):
+        return (1 << int(self.universe_width(U) / 2)) - 1
--- vEB/veb_test.py
+++ vEB/veb_test.py
@@ -4,7 +4,37 @@ from veb import Veb
 class TestStringMethods(unittest.TestCase):
 
     def test_ctor(self):
-        a = Veb(8)
+        b = Veb(8)
+        b.insert(6)
+        assert b.pred(7) == 6
+        assert b.succ(1) == 6
+        
+    def test_big_numbers(self):
+        u = 1 << 31
+        a = Veb(u)
+        print(a.U)
+        a.insert(1)
+        a.insert(12345)
+        a.insert(99999999)
+        a.insert(750000)
+        a.insert(750001)
+        a.insert((1 << 16))
+        a.insert((1 << 31) - 1)
+        
+        print("Pred of 2 is 1")
+        assert a.pred(2) == 1
+        print("b")
+        assert a.succ(2) == 12345
+        print("c")
+        assert a.succ(99999998) == 99999999
+        print("d")
+        assert a.pred(100000000) == 99999999
+        print("e")
+        assert a.pred(750001) == 750000
+        print("f")
+        assert a.pred((1 << 17)) == 1 << 16
+        print("g")
+        assert a.succ((1 << 30)) == (1 << 31) - 1
         
 if __name__ == '__main__':
     unittest.main()
