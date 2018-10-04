7a6d920862680cfb3677af7bc38ab9a1ded7613e
Troy Whorten <sabotuer99@gmail.com>
2018-10-04 06:53:54 +0000

    made all my veb tests pass

--- vEB/veb.py
+++ vEB/veb.py
@@ -2,13 +2,16 @@ from math import log
 from math import ceil
 
 class Veb:
+    def __repr__(self):
+        return "Veb(min_x: {}, max_x:{}, width: {})".format(self.min_x, self.max_x, self.width)
+    
     def __init__(self, U):
-        self.U = (1 << self.universe_width(U)) - 1
-        print(self.U)
+        self.width = self.universe_width(U)
+        self.U = (1 << self.width) - 1
         self.max_x = None
         self.min_x = None
-        m2 = self.halve_universe(self.U)
-        if(m2 > 1):
+        if(self.width > 1):
+            m2 = self.halve_universe()
             self.summary = Veb(m2)
             self.clusters = {}
     
@@ -19,15 +22,22 @@ class Veb:
             self.min_x = x
             self.max_x = x
             return
+        if self.width == 1: # one bit veb is special case
+            self.min_x = min(self.min_x, x)
+            self.max_x = max(self.max_x, x)
+            return
+        if self.min_x == x or self.max_x == x: # no duplicates
+            return
         to_insert = x
         if x < self.min_x:
             to_insert = self.min_x
             self.min_x = x
         if to_insert > self.max_x:
             self.max_x = to_insert
+            
         c, i = self.split_bits(to_insert)
         if c not in self.clusters:
-            self.clusters[c] = Veb(self.halve_universe(self.U))
+            self.clusters[c] = Veb(self.halve_universe())
             self.summary.insert(c)
         self.clusters[c].insert(i)
         
@@ -36,21 +46,20 @@ class Veb:
             return None
         if x > self.max_x:
             return self.max_x
+            
+        if self.width == 1 or self.min_x == self.max_x:
+            return self.min_x
+            
         c, i = self.split_bits(x)
         hi = x - i
-        half_width = int(self.universe_width(self.U) / 2)
-        print("c: " + str(c))
-        print("i: " + str(i))
-        print("hi: " + str(hi))
-        print(self.summary.clusters)
-        print("min: " + str(self.min_x))
-        print("max: " + str(self.max_x))
-        print(c in self.clusters)
-        print(self.clusters[c].min_x)
-        if c in self.clusters and self.clusters[c].min_x < x:
+        
+        if c in self.clusters and self.clusters[c].min_x < i:
             return hi + self.clusters[c].pred(i)
         else:
             cid = self.summary.pred(c)
+            if cid == None:
+                return self.min_x
+            hi = cid << (self.width // 2)
             return hi + self.clusters[cid].max_x
             
     def succ(self, x):
@@ -58,23 +67,27 @@ class Veb:
             return None
         if x < self.min_x:
             return self.min_x
+            
+        if self.width == 1 or self.min_x == self.max_x:
+            return self.max_x
+            
         c, i = self.split_bits(x)
         hi = x - i
-        half_width = int(self.universe_width(self.U) / 2)
-        print("c: " + str(c))
-        print("i: " + str(i))
-        print("hi: " + str(hi))
-        print(self.summary.clusters)
-        print("min: " + str(self.min_x))
-        print("max: " + str(self.max_x))
-        print(c in self.clusters)
-        if c in self.clusters and self.clusters[c].max_x > x:
+        
+        if c in self.clusters and self.clusters[c].max_x > i:
             return hi + self.clusters[c].succ(i)
         else:
             cid = self.summary.succ(c)
+            hi = cid << (self.width // 2)
             return hi + self.clusters[cid].min_x
             
     def universe_width(self, x):
+        # deal with edge cases
+        if x in (0, 1):
+            return 1
+        if x == 2:
+            return 2
+        
         width = ceil(log(x, 2))
         next_pow_2_width = ceil(log(width, 2))
         return 1 << next_pow_2_width
@@ -83,13 +96,17 @@ class Veb:
         half_width = int(self.universe_width(self.U) / 2)
         mask_right =  (1 << half_width) - 1
         mask_left = mask_right << half_width
-        
-        print(mask_left)
-        print(mask_right)
-        
         c = (x & mask_left) >> half_width
         i = x & mask_right
         return c, i
         
-    def halve_universe(self, U):
-        return (1 << int(self.universe_width(U) / 2)) - 1
+    def halve_universe(self):
+        h = (1 << int(self.width / 2)) - 1
+        return h
+        
+    def print_ones(self, x):
+        ones = ""
+        for i in reversed(range(self.width)):
+            val = (x >> i) & 1
+            ones += str(val)
+        return ones
--- vEB/veb_test.py
+++ vEB/veb_test.py
@@ -6,13 +6,88 @@ class TestStringMethods(unittest.TestCase):
     def test_ctor(self):
         b = Veb(8)
         b.insert(6)
-        assert b.pred(7) == 6
-        assert b.succ(1) == 6
+        self.assertEqual(b.pred(7), 6)
+        self.assertEqual(b.succ(1), 6)
+        
+    def test_structure(self):
+        sut = Veb(15)
+        sut.insert(0)
+        sut.insert(10)
+        # 1010 -> 10 10
+        # insert have summary entry for 2
+        self.assertEqual(2, sut.summary.max_x)
+        # 2 inserted in cluster 2
+        self.assertTrue(2, sut.clusters[2].max_x)
+        
+        sut.insert(11)
+        self.assertTrue(3, sut.clusters[2].max_x)
+        self.assertTrue(1, sut.clusters[2].clusters[1].max_x)
+
+    def test_1wide(self):
+        sut = Veb(1)
+        sut.insert(0)
+        self.assertEqual(sut.succ(0), None)
+        self.assertEqual(sut.succ(1), None)
+        self.assertEqual(sut.pred(0), None)
+        self.assertEqual(sut.pred(1), 0)    
+        
+        sut.insert(1)
+        self.assertEqual(sut.succ(0), 1)
+        self.assertEqual(sut.succ(1), None)
+        self.assertEqual(sut.pred(0), None)
+        self.assertEqual(sut.pred(1), 0)        
+        
+        
+    def test_2wide(self):
+        sut = Veb(3)
+        sut.insert(2)
+        self.assertEqual(sut.succ(0), 2)
+        self.assertEqual(sut.succ(1), 2)
+        self.assertEqual(sut.succ(2), None)
+        self.assertEqual(sut.succ(3), None)
+        
+        self.assertEqual(sut.pred(0), None)
+        self.assertEqual(sut.pred(1), None)
+        self.assertEqual(sut.pred(2), None)
+        self.assertEqual(sut.pred(3), 2)
+        
+        sut.insert(3)
+        self.assertEqual(sut.succ(0), 2)
+        self.assertEqual(sut.succ(1), 2)
+        self.assertEqual(sut.succ(2), 3)
+        self.assertEqual(sut.succ(3), None)
+        
+        self.assertEqual(sut.pred(0), None)
+        self.assertEqual(sut.pred(1), None)
+        self.assertEqual(sut.pred(2), None)
+        self.assertEqual(sut.pred(3), 2)
+        
+        sut.insert(0)
+        self.assertEqual(sut.succ(0), 2)
+        self.assertEqual(sut.succ(1), 2)
+        self.assertEqual(sut.succ(2), 3)
+        self.assertEqual(sut.succ(3), None)
+        
+        self.assertEqual(sut.pred(0), None)
+        self.assertEqual(sut.pred(1), 0)
+        self.assertEqual(sut.pred(2), 0)
+        self.assertEqual(sut.pred(3), 2)
+        
+        sut.insert(1)
+        self.assertEqual(sut.succ(0), 1)
+        self.assertEqual(sut.succ(1), 2)
+        self.assertEqual(sut.succ(2), 3)
+        self.assertEqual(sut.succ(3), None)
+        
+        self.assertEqual(sut.pred(0), None)
+        self.assertEqual(sut.pred(1), 0)
+        self.assertEqual(sut.pred(2), 1)
+        self.assertEqual(sut.pred(3), 2)
+        
         
     def test_big_numbers(self):
         u = 1 << 31
         a = Veb(u)
-        print(a.U)
         a.insert(1)
         a.insert(12345)
         a.insert(99999999)
@@ -21,20 +96,56 @@ class TestStringMethods(unittest.TestCase):
         a.insert((1 << 16))
         a.insert((1 << 31) - 1)
         
-        print("Pred of 2 is 1")
-        assert a.pred(2) == 1
-        print("b")
-        assert a.succ(2) == 12345
-        print("c")
-        assert a.succ(99999998) == 99999999
-        print("d")
-        assert a.pred(100000000) == 99999999
-        print("e")
-        assert a.pred(750001) == 750000
-        print("f")
-        assert a.pred((1 << 17)) == 1 << 16
-        print("g")
-        assert a.succ((1 << 30)) == (1 << 31) - 1
+        self.assertEqual(a.pred(2), 1)
+        self.assertEqual(a.succ(2), 12345)
+        self.assertEqual(a.succ(99999998), 99999999)
+        self.assertEqual(a.pred(100000000), 99999999)
+        self.assertEqual(a.pred(750001), 750000)
+        self.assertEqual(a.pred((1 << 17)), 1 << 16)
+        self.assertEqual(a.succ((1 << 30)), (1 << 31) - 1)
+        
+    def test_split_bits(self):
+        a = Veb(15) # 4 bits
+        self.assertEqual(a.split_bits(15), (3, 3)) # 1111 -> 11 11
+        self.assertEqual(a.split_bits(10), (2, 2)) # 1010 -> 10 10
+        self.assertEqual(a.split_bits(12), (3, 0)) # 1100 -> 11 00
+        
+        b = Veb(255) # 8 bits
+        self.assertEqual(b.split_bits(255), (15, 15)) # 11111111 -> 1111 1111
+        self.assertEqual(b.split_bits(0), (0, 0))   # 00000000 -> 0000 0000
+        self.assertEqual(b.split_bits(15), (0, 15))   # 00001111 -> 0000 1111
+        
+        c = Veb(3)
+        self.assertEqual(c.split_bits(3), (1, 1))
+        self.assertEqual(c.split_bits(2), (1, 0))
+        self.assertEqual(c.split_bits(1), (0, 1))
+        
+    def test_halve_universe(self):
+        a = Veb(15) # 4 bits
+        self.assertEqual(a.halve_universe(), 3) # 1111 -> 11
+        
+        b = Veb(255) # 8 bits
+        self.assertEqual(b.halve_universe(), 15) # 11111111 -> 1111
+        
+        c = Veb(3) # 2 bits
+        self.assertEqual(c.halve_universe(), 1) # 11 -> 1
+        
+    def test_print_ones(self):
+        a = Veb(15)
+        self.assertEqual(a.print_ones(15), '1111')
+        self.assertEqual(a.print_ones(10), '1010')
+        self.assertEqual(a.print_ones(3), '0011')
+        self.assertEqual(a.print_ones(0), '0000')
+        
+    def test_universe_width(self):
+        a = Veb(15)
+        self.assertEqual(a.universe_width(15), 4)
+        self.assertEqual(a.universe_width(5), 4)
+        self.assertEqual(a.universe_width(3), 2)
+        self.assertEqual(a.universe_width(2), 2)
+        self.assertEqual(a.universe_width(1), 1)
+    
+        
         
 if __name__ == '__main__':
     unittest.main()
